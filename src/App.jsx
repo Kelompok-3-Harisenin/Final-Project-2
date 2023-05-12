f681df775378261b814ee83b9f28e346bafa49af
jollyfitrah93 <jollyfitrahbilitoni@gmail.com>
2023-05-12 22:58:24 +0700

    export footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Homepage from './components/Homepage'
 import Footer from './components/Footer'
 import Login from './components/Login'
+import "@fortawesome/fontawesome-free/css/all.css";
+
+
 
 function App() {
 
@@ -11,6 +14,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Homepage/>}></Route>
+         
           <Route path='/login' element={<Login/>}></Route>
         </Routes>
       </Router>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import  '../styles/index.css';
+import "@fortawesome/fontawesome-free/css/all.css";
 
-const Footer = () => {
+
+
+
+function Footer() {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='flex justify-center items-center px-6 max-md:flex-col max-md:text-center pb -7'>
+      <div className='py-4 justify-center ml-24 max-md:ml-0 max-md:text-center'>
+        <a className='block hover:text-gray-700' href="#">Contact Us</a>
+        <a className='block hover:text-gray-700' href="#">How To Shop</a>
+        <a className='block hover:text-gray-700' href="#">FAQ</a>
+        <a className='block hover:text-gray-700' href="#">Return Policy</a>
+        <a className='block hover:text-gray-700' href="#">Size Guide</a>
+      </div>
+      <div className='py-4 md:flex-row ml-24 max-md:ml-0 max-md:text-center'>
+        <a className='block hover:text-gray-700' href="#">Terms & Condition</a>
+        <a className='block hover:text-gray-700' href="#">Terms of Service</a>
+        <a className='block hover:text-gray-700' href="#">Refund Policy</a>
+        <a className='block hover:text-gray-700' href="#">Order Confirmation</a>
+      </div>
+      <div className='flex-auto ml-44 max-md:ml-0 py-4'>
+        <p><a className='fab fa-facebook hover:text-gray-700 text-xs' href="#"> Facebook</a></p>
+        <p><a className='fab fa-twitter hover:text-gray-700 text-xs' href="#"> Twitter</a></p>
+        <p><a className='fab fa-instagram hover:text-gray-700 text-xs' href="#">  Instagram</a></p>
+        <p><a className='fab fa-youtube hover:text-gray-700 text-xs' href="#">  Youtube</a></p>
+      </div>
+      <div className='py-4 max-md:mr-0 mr-4'>
+        <p>&copy;2023, <a class="hover:text-gray-700" href="#">Screamous / Daily Wear</a></p>
+        <img src="assets/Homepage/Screamous.jpg" alt="logo screamous" className='w-20 h-20 max-md:ml-12 ml-36 mt-2'/>
+          <img src="assets/Homepage/channel pembayaran.PNG" alt="channel pembayaran" className='max-md:-ml-5'/>
+          </div>
+        </div>
+      )
+      }
 
-export default Footer
+      export default Footer;
