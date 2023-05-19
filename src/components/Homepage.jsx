fa72d576c5d54a2233f2f158d1cd7e367b57ca24
Naufal Nur Halim <naufalnh77@gmail.com>
2023-05-19 23:05:40 +0700

    Penambahan File

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,24 +4,18 @@ import Header from './Header'
 const Homepage = () => {
   return (
     <>
-
-<body>
-<div class="hero-img min-h-screen bg-cover bg-no-repeat bg-center">
-  <Header/>
-      <div class="hero-content w-full h-screen flex justify-center">
-        <a
-          href=""
-          class="button my-auto text-white border bg-[#1f2021] py-3 px-5 font-medium"
-        >
-          SHOP NOW
-        </a>
-      </div>
-    </div>
-
-
-</body>
+      <body>
+        <div class="hero-img min-h-screen bg-cover bg-no-repeat bg-center">
+          <Header />
+          <div class="hero-content w-full h-screen flex justify-center">
+            <a href="" class="button my-auto text-white border bg-[#1f2021] py-3 px-5 font-medium">
+              SHOP NOW
+            </a>
+          </div>
+        </div>
+      </body>
     </>
-    )
+  )
 }
 
 export default Homepage
