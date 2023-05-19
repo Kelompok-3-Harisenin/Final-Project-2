f63ab7b0535738df9accd8f83307c4122f71b6f7
Naufal Nur Halim <naufalnh77@gmail.com>
2023-05-20 00:55:13 +0700

    merubah file app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-// import { useState } from 'react'
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import Homepage from './components/Homepage'
-// import Footer from './components/Footer'
-// import Login from './components/Login'
-// import Header from './components/Header'
-import Magazine from './components/Magazine'
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Homepage from './components/Homepage'
+import Footer from './components/Footer'
+import Login from './components/Login'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      {/* <Router>
+      <Router>
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
           <Route path="/login" element={<Login />}></Route>
         </Routes>
       </Router>
-      <Footer /> */}
-      <Magazine />
+      <Footer />
     </>
   )
 }
